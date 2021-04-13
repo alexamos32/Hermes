454501a3a497a54ee748af0fb427d27c56d70e81
Alex Amos <alexanderqamos@gmail.com>
2021-04-13 04:33:09 -0300

    Update DB version

--- app/src/main/java/ca/unb/mobiledev/hermes/NoteDatabase.java
+++ app/src/main/java/ca/unb/mobiledev/hermes/NoteDatabase.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class NoteDatabase extends SQLiteOpenHelper{
 
-    private static final int DATABASE_VERSION = 3;
+    private static final int DATABASE_VERSION = 4;
     private static final String DATABASE_NAME = "notesdb";
     private static final String DATABASE_TABLE = "notestable";
 
